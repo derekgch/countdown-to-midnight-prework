0efe821acb82d1838dd83a5b95059289d3456101
derek chen <derek.g.chen@gmail.com>
2018-03-20 22:09:09 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown (second)
+  while second >0 do
+    puts "#{second} SECOND(S)!"
+    second -= 1
+    countdown_with_sleep(second)
+  end  
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  sleep(1)
 end
